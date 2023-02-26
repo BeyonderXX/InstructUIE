90c74a78f09694524440c90d34887ad007c70fd7
waltersumbon <907449377@qq.com>
2023-02-26 15:09:28 +0800

    fix F1 score

--- src/evaluation/eval_main.py
+++ src/evaluation/eval_main.py
@@ -6,8 +6,6 @@ __all__ = [
 ]
 
 
-# TODO, xia han, bug fix
-# f1 score 应该在数据集集合尺度上计算 precision、recall，而不是对单个样本计算，之后求平均
 def eval(json_data, y_preds, task: str):
     """
     integrated evaluation entry
@@ -23,28 +21,49 @@ def eval(json_data, y_preds, task: str):
         'MRC': eval_MRC,
         'SM': eval_SM
     }
+    metric_dict = {
+        'NER': 'f1',
+        'RE': 'f1',
+        'Event': 'f1',
+        'MRC': 'acc',
+        'SM': 'acc'
+    }
     assert task in func_dict, 'Invalid task name %s.'%(task)
-    scores = []
-    for json_datus, y_pred in zip(json_data, y_preds):
-        score = func_dict[task](json_datus, y_pred)
-        scores.append(score)
-    return sum(scores) / len(scores)
 
+    metric = metric_dict[task]
+
+    if metric == 'acc':
+        scores = []
+        for json_datus, y_pred in zip(json_data, y_preds):
+            score = func_dict[task](json_datus, y_pred)
+            scores.append(score)
+        return sum(scores) / len(scores)
+    elif metric == 'f1':
+        # f1 score 应该在数据集集合尺度上计算 precision、recall，而不是对单个样本计算，之后求平均
+        sum_TP = sum_FN = sum_FP = 0
+        for json_datus, y_pred in zip(json_data, y_preds):
+            TP, FN, FP = func_dict[task](json_datus, y_pred)
+            sum_TP += TP
+            sum_FN += FN
+            sum_FP += FP
+        return _calc_f1_score(sum_TP, sum_FN, sum_FP)
 
 def _remove_redundant_space(s):
     # '   a  b  \t  c  \n' --> 'a b c'
     return ' '.join(s.split())
 
-
-def _calc_f1_score(truth: set, pred: set):
-    # TP: 在data中存在，且在输出中存在
-    # FN: 在data中存在，但在输出中不存在
-    # FP: 在data中不存在，但在输出中存在
-    # TP + FN 必不为0
-    # TP + FP 可能为0
+def _TFPN(truth: set, pred: set):
+    # TP: 在truth中存在，且在pred中存在
+    # FN: 在truth中存在，但在pred中不存在
+    # FP: 在truth中不存在，但在pred中存在
     TP = len(truth & pred)
     FN = len(truth - pred)
     FP = len(pred - truth)
+    return TP, FN, FP
+
+def _calc_f1_score(TP, FN, FP):
+    # TP + FN 可能为0
+    # TP + FP 可能为0
     if TP + FN == 0:
         recall = 0
     else:
@@ -64,7 +83,7 @@ def eval_NER(json_data, y_pred: str):
     """
     json_data: json-like object
     y_pred: single string outputed by model
-    return: F1 score 
+    return: TP, FN, FP
     """
     entity_truth = set()
     for ent in json_data['entities']:
@@ -81,26 +100,29 @@ def eval_NER(json_data, y_pred: str):
             ent = _remove_redundant_space(ent)
             entity_pred.add(ent)
     
-    return _calc_f1_score(entity_truth, entity_pred)
+    return _TFPN(entity_truth, entity_pred)
 
 
 def eval_RE(json_data, y_pred: str):
     """
     json_data: json-like object
     y_pred: single string outputed by model
-    return: F1 score 
+    return: TP, FN, FP
     """
     truth = set()
     for rel in json_data['relations']:
         head = rel['head']['name']
         rel_type = rel['type']
         tail = rel['tail']['name']
-        rel = '(%s,%s,%s)'%(head, rel_type, tail)
-        rel = _remove_redundant_space(rel)
-        truth.add(rel)
+        if rel_type != 'no_relation':
+            # 数据集中type为'no_relation'的关系会被全部忽略掉
+            rel = '(%s,%s,%s)'%(head, rel_type, tail)
+            rel = _remove_redundant_space(rel)
+            truth.add(rel)
     
     pred = set()
     if y_pred.strip() != 'no relation':
+        # 如果模型输出'no relation'，则认为其预测的关系集合为空集
         for rel in re.findall(r'\(.+?\)', y_pred):
             rel = rel[1:-1]
             elements = tuple([i.strip() for i in rel.split(',')])
@@ -109,7 +131,7 @@ def eval_RE(json_data, y_pred: str):
                 rel = '(%s,%s,%s)'%elements
                 rel = _remove_redundant_space(rel)
                 pred.add(rel)
-    return _calc_f1_score(truth, pred)
+    return _TFPN(truth, pred)
 
 
 def eval_MRC(json_data, y_pred: str):
@@ -134,10 +156,17 @@ def eval_SM(json_data, y_pred: str):
     """
     y = _remove_redundant_space(json_data['label'])
     y_pred = _remove_redundant_space(y_pred)
-    if y_pred == '是':
-        y_pred = 'Yes'
-    elif y_pred == '否':
-        y_pred = 'No'
+    trans_dict = {
+        '是': 'Yes',
+        '否': 'No',
+        'yes': 'Yes',
+        'no': 'No'
+    }
+    if y in trans_dict:
+        y = trans_dict[y]
+    if y_pred in trans_dict:
+        y_pred = trans_dict[y_pred]
+
     if y_pred == y:
         return 1
     else:
@@ -148,7 +177,7 @@ def eval_Event(json_data, y_pred: str):
     """
     json_data: json-like object
     y_pred: single string outputed by model
-    return: F1 score
+    return: TP, FN, FP
     """
     truth = set()
     for event in json_data['events']:
@@ -185,14 +214,14 @@ def eval_Event(json_data, y_pred: str):
             event_string = _remove_redundant_space(event_string)
             pred.add(event_string)
     print(truth, pred)
-    return _calc_f1_score(truth, pred)
+    return _TFPN(truth, pred)
 
 
 if __name__ == '__main__':
     print(
-        eval_NER(
-            """
-            {
+        eval(
+            json.loads("""
+            [{
                 "sentence": "我在复旦大学上学，想去墨脱喝奶茶。",
                 "entities": [
                     {
@@ -212,13 +241,15 @@ if __name__ == '__main__':
                         ]
                     }
                 ]
-            }
-                    """,
-            "organization: 复旦大学, location: 墨脱"
+            }]
+            """),
+            ["organization: 复旦大学, location: 墨脱"],
+            task = 'NER'
         ),
-        eval_RE(
-            """
-            {
+        'Expected Result: 1.0',
+        eval(
+            json.loads("""
+            [{
                 "sentence": "我在复旦大学上学，想去墨脱喝奶茶。",
                 "relations": [
                     {
@@ -242,12 +273,7 @@ if __name__ == '__main__':
                                 }
                     }
                 ]
-            }
-            """,
-            'no relation'
-        ),
-        eval_RE(
-            """
+            },
             {
                 "sentence": "小明在上海的复旦大学上学。",
                 "relations": [
@@ -293,12 +319,15 @@ if __name__ == '__main__':
                     }
                 ]
             }
-            """,
-            '(复旦大学, locate  in, 上海), (小学,belong to, 复旦大学)'
+            ]
+            """),
+            ['no relation', '(复旦大学, locate  in, 上海), (李田所,belong to, 复旦大学)'],
+            task = 'RE'
         ),
-        eval_Event(
-            """
-            {
+        'Expected Result: 0.5',
+        eval(
+            json.loads("""
+            [{
                 "sentence": "雀巢裁员4000人：时代抛弃你时，连招呼都不会打！",
                 "events": [
                     {
@@ -328,40 +357,42 @@ if __name__ == '__main__':
                         ]
                     }
                 ]
-            }
-            """,
-            "(event type : 组织关系-裁员, trigger: 裁员, 裁员方: 雀巢, 裁员人数: 4000人), (event type : 组织关系-裁员, trigger: 裁员, 裁员方: 雀巢, 裁员人数: 3000人)"
+            }]
+            """),
+            ["(event type : 组织关系-裁员, trigger: 裁员, 裁员方: 雀巢, 裁员人数: 4000人), (event type : 组织关系-裁员, trigger: 裁员, 裁员方: 雀巢, 裁员人数: 3000人)"],
+            task = 'Event'
         ),
-        eval_MRC(
-            """
-            {
+        'Expected Result: 2/3',
+        eval(
+            json.loads("""
+            [{
                 "paragraph": "Another approach to brain function is to examine the consequences of damage to specific brain areas. Even though it is protected by the skull and meninges, surrounded by cerebrospinal fluid, and isolated from the bloodstream by the blood\u2013brain barrier, the delicate nature of the brain makes it vulnerable to numerous diseases and several types of damage. In humans, the effects of strokes and other types of brain damage have been a key source of information about brain function. Because there is no ability to experimentally control the nature of the damage, however, this information is often difficult to interpret. In animal studies, most commonly involving rats, it is possible to use electrodes or locally injected chemicals to produce precise patterns of damage and then examine the consequences for behavior.",
-                    "question": "What sare the benifts of the blood brain barrir?",
-                    "answer_start": 195,
-                    "answer_text": "isolated from the bloodstream"
-            }
-            """,
-            "isolated from the bloodstream"
+                "question": "What sare the benifts of the blood brain barrir?",
+                "answer_start": 195,
+                "answer_text": "isolated from the bloodstream"
+            }]
+            """),
+            ["isolated from the bloodstream"],
+            task = 'MRC'
         ),
-        eval_SM(
-            """
-            {
+        'Expected Result: 1.0',
+        eval(
+            json.loads("""
+            [{
                 "sen1": "A man with a hard hat is dancing.",
                 "sen2": "A man wearing a hard hat is dancing.",
                 "label": "Yes"
-            }
-            """,
-            "Yes"
-        ),
-        eval_SM(
-            """
+            },
             {
                 "sen1": "蚂蚁借呗等额还款可以换成先息后本吗",
                 "sen2": "借呗有先息到期还本吗",
                 "label": "否"
             }
-            """,
-            "No"
+            ]
+            """),
+            ["是", "no"],
+            task = 'SM'
         ),
+        'Expected Result: 1.0',
         sep='\n'
     )
