fa71c97daf0a5487da6e6be9753cc05829c5c5bd
waltersumbon <907449377@qq.com>
2023-03-05 13:02:21 +0800

    git ignore hostfile; update setup.sh; multi-node

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 pip install -r requirements.txt
 apt-get -y install ninja-build
+apt-get -y install pdsh
 python -c "import nltk; nltk.download('punkt', quiet=True)"
-conda install mpi4py
+conda install -y mpi4py
 # wget https://github.com/ninja-build/ninja/releases/download/v1.10.2/ninja-linux.zip
 # sudo unzip ninja-linux.zip -d /usr/local/bin/
