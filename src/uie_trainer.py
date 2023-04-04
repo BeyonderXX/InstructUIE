49c0912606b717556214bf732381ad6e2ebd0495
夏寒 <xiahan.zenith@bytedance.com>
2023-04-04 15:58:16 +0800

    <human> rather than human

--- src/uie_trainer.py
+++ src/uie_trainer.py
@@ -279,7 +279,7 @@ class UIETrainer(Seq2SeqTrainer):
         generated_tokens = self.model.generate(
             generation_inputs,
             generation_config=generation_config,
-            eos_token_id=self.tokenizer.convert_tokens_to_ids("human")
+            eos_token_id=self.tokenizer.convert_tokens_to_ids("<human>:")
         )
 
         bs, source_len = inputs['input_ids'].shape
