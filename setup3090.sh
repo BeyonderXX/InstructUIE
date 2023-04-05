72236fff1e78f40321fe2235d40034a0c1672efd
weikang zhou <57825291+nitwtog@users.noreply.github.com>
2023-04-05 15:51:25 +0800

    Create setup3090.sh
    在自己的服务器上可以一键配置

---
+++ setup3090.sh
@@ -0,0 +1,23 @@
+pip install transformers==4.26.1
+pip install datasets==1.17.0
+pip install deepspeed==0.8.0
+pip install fairscale==0.4.5
+pip install ipython
+pip install nltk
+pip install tensorboard
+pip install tqdm
+pip install rouge_score
+pip install wandb==0.12.10
+pip install sentencepiece==0.1.96
+pip install protobuf==3.20.3
+pip install accelerate
+pip install ninja
+#sudo yum -y install ninja-build
+apt -y install ninja-build
+apt -y install tmux
+python -c "import nltk; nltk.download('punkt', quiet=True)"
+conda install mpi4py
+#sudo yum -y install centos-release-scl
+#sudo yum -y install devtoolset-7-gcc*
+#sudo scl enable devtoolset-7 bash
+#sudo which gcc
